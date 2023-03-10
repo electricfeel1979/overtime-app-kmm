af233c6cb68b183f6e26835d61e88c9a5569c78a
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-03-11 01:00:08 +0800

    16. Building a Relational Connection Between Users and Posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'Creation' do
     before do
-      @post = Post.create(date: Date.today, rationale: 'Anything')
+      @user = User.create(:first_name => 'Buru', :last_name => 'gudoy', email: 'test@test.com', password: '123123', password_confirmation: '123123')
+      # debugger
+      @post = Post.create(date: Date.today, rationale: 'Anything', user_id: @user.id)
     end
     it 'can be created' do
       expect(@post).to be_valid
