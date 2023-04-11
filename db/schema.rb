ceed8e2c2bbd4fefb14fd012bc81fa2bf18ceaaa
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-04-11 14:25:28 +0800

    56. Adding Overtime Hours to Posts with Validations && 57. Updating Integration Tests to Include Requested Overtime Hours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_061426) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_054655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_061426) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.integer "status", default: 0
+    t.decimal "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate' do
   let(:user) { FactoryBot.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id, overtime_request: 3.5)
   end
 
   before do
@@ -34,7 +34,7 @@ describe 'navigate' do
 
     it 'has a scope so that only post creators can see their posts' do
       non_authorized_user = FactoryBot.build_stubbed(:non_authorized_user)
-      FactoryBot.build_stubbed(:post_from_other_user, rationale: "This post should'nt be seen", user: non_authorized_user)
+      FactoryBot.build_stubbed(:post_from_other_user, rationale: "This post should'nt be seen", user: non_authorized_user, overtime_request: 3.5)
 
       visit posts_path
 
@@ -58,7 +58,7 @@ describe 'navigate' do
       delete_user = FactoryBot.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -76,17 +76,17 @@ describe 'navigate' do
     end
 
     it 'can be created from new form page' do
-      # debugger
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-      click_on 'Save'
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content("Some rationale")
+      expect { click_on "Save" }.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'User Association'
+      fill_in 'post[overtime_request]', with: 4.5
       click_on 'Save'
 
       expect(User.last.posts.last.rationale).to eq('User Association')
