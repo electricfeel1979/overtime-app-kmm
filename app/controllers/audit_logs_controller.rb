c2c72bf1f73ca5677dc4277ce549fe0ed085d413
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-05-01 11:51:15 +0800

    89. Building Instant Approval Functionality for Employees

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,11 +1,18 @@
 class AuditLogsController < ApplicationController
-    def index
-      @audit_logs = AuditLog.page(params[:page]).per(10)
-      authorize @audit_logs
+  def index
+    @audit_logs = AuditLog.page(params[:page]).per(10)
+    authorize @audit_logs
 
-      respond_to do |format|
-        format.html
-        format.js
-      end
+    respond_to do |format|
+      format.html
+      format.js
     end
+  end
+
+  def confirm
+    audit_log = AuditLog.find(params[:id])
+    authorize audit_log
+    audit_log.confirmed!
+    redirect_to root_path, notice: "Thank you, your confirmation has been successfully made."
+  end
 end
