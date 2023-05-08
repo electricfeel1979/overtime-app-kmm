8f7b6b5c52375f73b3693022f526335bb2e6a9a5
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-05-08 17:53:46 +0800

    107. Adding Single Table Inheritance Class to Administrative Dashboard

---
+++ app/dashboards/employee_dashboard.rb
@@ -0,0 +1,54 @@
+require "administrate/base_dashboard"
+
+class EmployeeDashboard < Administrate::BaseDashboard
+  ATTRIBUTE_TYPES = {
+    posts: Field::HasMany.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    email: Field::String.with_options(searchable: true),
+    password: Field::String.with_options(searchable: false),
+    encrypted_password: Field::String.with_options(searchable: false),
+    first_name: Field::String.with_options(searchable: false),
+    last_name: Field::String.with_options(searchable: false),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    remember_created_at: Field::DateTime.with_options(searchable: false),
+    reset_password_sent_at: Field::DateTime.with_options(searchable: false),
+    type: Field::String.with_options(searchable: false),
+    reset_password_token: Field::String.with_options(searchable: false),
+    phone: Field::String.with_options(searchable: false),
+  }.freeze
+
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    email
+    phone
+    encrypted_password
+    first_name
+  ].freeze
+
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    email
+    encrypted_password
+    first_name
+    last_name
+    posts
+    remember_created_at
+    reset_password_sent_at
+    reset_password_token
+    type
+    created_at
+    updated_at
+  ].freeze
+
+  FORM_ATTRIBUTES = %i[
+    email
+    password
+    first_name
+    last_name
+    type
+    phone
+  ].freeze
+
+  COLLECTION_FILTERS = {}.freeze
+end
