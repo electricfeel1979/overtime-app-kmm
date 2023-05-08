1e0c8f51165d6704198597b5402046a1d50ceb33
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-05-08 11:29:15 +0800

    98. Building Rake Task for Sending SMS Messages to Employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: 'test@yahoo.com', 
+@employee = Employee.create(email: 'test@yahoo.com', 
                     password: '123123', 
                     password_confirmation: '123123', 
                     first_name: 'Kevin', 
                     last_name: 'Montesclaros',
                     phone: '1111111111')
 
-puts "1 User created"
+puts "1 Employee created"
 
 AdminUser.create(email: 'admin@admin.com', 
             password: '123123', 
@@ -16,9 +16,9 @@ AdminUser.create(email: 'admin@admin.com',
 
 puts "1 Admin User created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
@@ -27,7 +27,7 @@ puts "3 audit logs have been created"
                rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-               user_id: @user.id, 
+               user_id: @employee.id, 
                overtime_request: 2.5)
 end
 
