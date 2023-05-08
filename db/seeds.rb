1d9f96b321f272bcc188e0c6ae88e9e9ba961578
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-05-08 09:40:54 +0800

    94. Deploying to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,12 @@ AdminUser.create(email: 'admin@admin.com',
 
 puts "1 Admin User created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
+
 100.times do |post|
   Post.create!(date: Date.today, 
                rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -26,9 +32,3 @@ puts "1 Admin User created"
 end
 
 puts "100 Posts have been created"
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit logs have been created"
