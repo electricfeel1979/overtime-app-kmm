38e39694d1780c08e7d1896880a00edf7b204755
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-05-09 20:06:19 +0800

    118. Migrating from Overtime Requests to a Full Time Tracking Solution

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,21 +2,21 @@ FactoryBot.define do
   factory :post do
     date { Date.today }
     sequence(:rationale) { |n| "Rationale Content #{n}" }
-    overtime_request { 3.5 }
+    daily_hours { 3.5 }
     user
   end
 
   factory :second_post, class: 'Post' do
     date { Date.yesterday }
     sequence(:rationale) { |n| "Rationale Content #{n}" }
-    overtime_request { 0.5 }
+    daily_hours { 0.5 }
     user
   end
 
   factory :post_from_other_user, class: 'Post' do
     date { Date.yesterday }
     sequence(:rationale) { |n| "Rationale Content #{n}" }
-    overtime_request { 0.5 }
+    daily_hours { 0.5 }
     user
   end
 end
